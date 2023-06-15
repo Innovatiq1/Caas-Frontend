cdf02afbc6c3802dba8188207c42eaf59de04058
Prem Kumar <sharad@innovatiq.com.sg>
2023-06-15 16:56:51 +0530

    home page structure

---
+++ CAAS/src/app/modules/student/student.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+
+const route: Routes = [
+  {
+        path: 'home', component: HomeComponent,
+
+  }
+
+]
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentModule { }
